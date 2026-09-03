37fd2a551d5dfc9c7a8116ba5fe5ec6750a54ca2
Alex Macharia <87443384+machariaalex@users.noreply.github.com>
2026-09-03 23:33:19 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
           onClick={(e) => handleNav(e, "/")}
           className="flex items-center transition-transform duration-300 hover:scale-[1.04]"
         >
-          <Logo size={56} />
+          <Logo size={45} />
         </Link>
 
         <div className="hidden items-center gap-9 lg:flex">
